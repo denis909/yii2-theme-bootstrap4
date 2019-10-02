79c64e36368f03fcd6b25072a500a4c738c6358d
denis909 <denis909@mail.ru>
2019-10-02 21:15:48 +0300

    n/a

--- ActiveForm.php
+++ ActiveForm.php
@@ -6,5 +6,16 @@ class ActiveForm extends \denis909\theme\ActiveForm
 {
 
     public $fieldClass = ActiveField::class;
+
+    public $defaultSubmitButtonOptions = [
+        'class' => 'btn btn-primary'
+    ];
+
+    public function submitButton($name, array $options = [])
+    {
+        $options = ArrayHelper::merge($this->defaultSubmitButtonOptions, $options);
+
+        return parent::submitButton($name, $options);
+    }
     
 }
